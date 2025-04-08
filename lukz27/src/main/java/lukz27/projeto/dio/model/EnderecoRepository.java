c39d392823895da26a97f6da6cfe16d48ea165ac
lukkaz2709 <lmiguez2709@gmail.com>
2025-04-08 13:38:02 -0300

    Projeto springboot

---
+++ lukz27/src/main/java/lukz27/projeto/dio/model/EnderecoRepository.java
@@ -0,0 +1,7 @@
+package lukz27.projeto.dio.model;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface EnderecoRepository extends CrudRepository<Endereco, String> {
+
+}
---
+++ lukz27/src/main/java/lukz27/projeto/dio/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package lukz27.projeto.dio.service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import lukz27.projeto.dio.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+    @GetMapping("/{cep}/json/")
+    Endereco consultarCep(@PathVariable("cep") String cep);
+}
